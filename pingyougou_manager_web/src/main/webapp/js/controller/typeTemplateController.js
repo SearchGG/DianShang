7cd0c583ad7839b35920cfddcafef80fa66feeca
fanyupeng <m18152286758@163.com>
2018-12-07 08:49:31 +0800

    模板新增删除修改功能实现

---
+++ pingyougou_manager_web/src/main/webapp/js/controller/typeTemplateController.js
@@ -0,0 +1,115 @@
+ //控制层 
+app.controller('typeTemplateController' ,function($scope,$controller   ,typeTemplateService,brandService,specificationService){
+	
+	$controller('baseController',{$scope:$scope});//继承
+	
+    //读取列表数据绑定到表单中  
+	$scope.findAll=function(){
+		typeTemplateService.findAll().success(
+			function(response){
+				$scope.list=response;
+			}			
+		);
+	}    
+	
+	//分页
+	$scope.findPage=function(page,rows){			
+		typeTemplateService.findPage(page,rows).success(
+			function(response){
+				$scope.list=response.rows;	
+				$scope.paginationConf.totalItems=response.total;//更新总记录数
+			}			
+		);
+	}
+	
+	//查询实体 
+	$scope.findOne=function(id){				
+		typeTemplateService.findOne(id).success(
+			function(response){
+				$scope.entity= response;
+				$scope.entity.brandIds=JSON.parse(response.brandIds);
+				$scope.entity.specIds=JSON.parse(response.specIds);
+				$scope.entity.customAttributeItems=JSON.parse(response.customAttributeItems);
+			}
+		);				
+	}
+	
+	//保存 
+	$scope.save=function(){				
+		var serviceObject;//服务层对象  				
+		if($scope.entity.id!=null){//如果有ID
+			serviceObject=typeTemplateService.update( $scope.entity ); //修改  
+		}else{
+			serviceObject=typeTemplateService.add( $scope.entity  );//增加 
+		}				
+		serviceObject.success(
+			function(response){
+				if(response.success){
+					//重新查询 
+		        	$scope.reloadList();//重新加载
+				}else{
+					alert(response.message);
+				}
+			}		
+		);				
+	}
+	
+	 
+	//批量删除 
+	$scope.dele=function(){			
+		//获取选中的复选框			
+		typeTemplateService.dele( $scope.selectIds ).success(
+			function(response){
+				if(response.success){
+					$scope.reloadList();//刷新列表
+				}						
+			}		
+		);				
+	}
+	
+	$scope.searchEntity={};//定义搜索对象 
+	
+	//搜索
+	$scope.search=function(page,rows){			
+		typeTemplateService.search(page,rows,$scope.searchEntity).success(
+			function(response){
+				$scope.list=response.rows;	
+				$scope.paginationConf.totalItems=response.total;//更新总记录数
+			}			
+		);
+	}
+
+    //品牌下拉列表
+    $scope.brandList={data:[]};//品牌列表
+	//读取品牌列表
+    $scope.findBrandList=function(){
+        brandService.selectOptionList().success(
+            function(response){
+                $scope.brandList={data:response};
+            }
+        );
+    }
+
+    //规格下拉列表
+    $scope.specList={data:[]};//品牌列表
+    //读取品牌列表
+    $scope.findSpecList=function(){
+        specificationService.selectOptionList().success(
+            function(response){
+                $scope.specList={data:response};
+            }
+        );
+    }
+    //新增扩展属性
+    //定义
+    $scope.entity={customAttributeItems:[]};
+
+    //新增扩展属性
+    $scope.addRow=function () {
+        $scope.entity.customAttributeItems.push({});
+    }
+    //删除扩展属性
+    $scope.deleRow=function (index) {
+        $scope.entity.customAttributeItems.splice(index,1);
+    }
+});	

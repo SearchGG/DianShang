a0810d359d9a7bc24909e3e624d57d2a46458804
fanyupeng <m18152286758@163.com>
2018-12-09 00:30:11 +0800

    商家入驻信息实现

--- pingyougou_sellersgoods_service/src/main/java/com/pinyougou/sellergoods/service/impl/SellerServiceImpl.java
+++ pingyougou_sellersgoods_service/src/main/java/com/pinyougou/sellergoods/service/impl/SellerServiceImpl.java
@@ -1,4 +1,5 @@
 package com.pinyougou.sellergoods.service.impl;
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.alibaba.dubbo.config.annotation.Service;
@@ -46,6 +47,11 @@ public class SellerServiceImpl implements SellerService {
 	 */
 	@Override
 	public void add(TbSeller seller) {
+		//商家状态为未审核 0
+		seller.setStatus("0");
+
+		//时间
+		seller.setCreateTime(new Date());
 		sellerMapper.insert(seller);		
 	}
 

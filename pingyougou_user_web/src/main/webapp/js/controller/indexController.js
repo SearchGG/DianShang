2f34276f5c946de3294815ccc5a947b385336333
fanyupeng <m18152286758@163.com>
2018-12-26 19:56:23 +0800

    单点登录

--- pingyougou_user_web/src/main/webapp/js/controller/indexController.js
+++ pingyougou_user_web/src/main/webapp/js/controller/indexController.js
@@ -1,11 +1,13 @@
  //控制层 
-app.controller('indexController' ,function($scope,$controller   ,contentService){
-	
-	$controller('baseController',{$scope:$scope});//继承
-	
-    $scope.findByCategoryId=function(catrgoryId) {
-		contentService.findByCategoryId(catrgoryId).success(function (response) {
-			$scope.contentList=response;
+app.controller('indexController' ,function($scope,$controller  ,loginService){
+
+    //控制器继承代码
+    $controller("baseController",{$scope:$scope});
+
+    $scope.getName=function () {
+        loginService.getName().success(function (response) {
+
+            $scope.loginName=response.loginName;
         })
-    }
+    };
 });	
